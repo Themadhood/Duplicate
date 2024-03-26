96685adb37268b7b36227d421094b0110700f759
Themadhood's Codes <148127209+Themadhood@users.noreply.github.com>
2024-03-26 16:09:56 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,15 +4,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='TPC_Error',
+    name='TPC_Duplicate',
     version="1.0.0",#__version__,
 
-    url='https://github.com/Themadhood/Error',
+    url='https://github.com/Themadhood/Duplicate',
     author='Themadhood Pequot',
     author_email='themadhoodpequot@gmail.com',
 
     packages=find_packages(),
-    #py_modules=['Error'],
 
     install_requires=[
         "gspread",#manipulates google sheets
